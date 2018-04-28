e4b9ed32f600c26f99047362b514b4a560dbe6d2
Ankit <ankit@qexon.com>
2018-04-28 21:36:44 +0530

    api changes

--- controllers/videoInfoController.js
+++ controllers/videoInfoController.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 var VideoInfo = require('../models/VideoInfoModel');
 
@@ -10,24 +11,23 @@ router.post('/video', function (req, res, next) {
             console.log(err);
         } else {
             console.log("successfull");
-            res.send({message : "saved"})
+            res.send({ message: "saved" })
         }
     });
 });
 
-router.get('/video', function (req, res, next) {
+router.get('/tags', function (req, res, next) {
     VideoInfo.find(function (err, data) {
         res.send(data);
     });
 });
 
-router.post('/videosbytag', function (req, res, next) {
-    //PersonModel.find({ "favouriteFoods.name": "Sushi" });
-    let tag = req.body.tag
-    VideoInfo.find({ "tags": tag }, function (err, data) {
+router.get('/videos', function (req, res, next) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query.tag;
+    VideoInfo.find({ "tags": query }, function (err, data) {
         res.send(data);
     });
-    //res.send('Api is running');
 });
 
 router.get('/test', function (req, res, next) {
