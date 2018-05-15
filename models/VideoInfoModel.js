337b523ca170a5482e5a0b7f82a1066c1d03063f
Ankit <ankit@qexon.com>
2018-05-15 10:30:39 +0530

    chnage api to get tags only

--- models/VideoInfoModel.js
+++ models/VideoInfoModel.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var videoSchema = new mongoose.Schema({
     url: String,
     tags: Array,
-    time: String
+    time: String,
+    status: {type : String, default : "A"},
 });
 
 module.exports = mongoose.model('video_info', videoSchema);
