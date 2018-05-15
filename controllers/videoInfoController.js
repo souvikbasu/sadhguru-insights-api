a2db97d86344d6aa7f099d614a1f0c870cf03ee7
Ankit <ankit@qexon.com>
2018-05-15 16:30:48 +0530

    add new api's for update and delete

--- controllers/videoInfoController.js
+++ controllers/videoInfoController.js
@@ -10,7 +10,6 @@ router.post('/video', function (req, res, next) {
         if (err) {
             console.log(err);
         } else {
-            console.log("successfull");
             res.send({ message: "saved" })
         }
     });
@@ -18,6 +17,10 @@ router.post('/video', function (req, res, next) {
 
 router.get('/tags', function (req, res, next) {
     VideoInfo.find({ status: "A" }, { tags: 1, status: 1 },function (err, data) {  
+        if (err) {
+            res.status(404).send(err);
+            return;
+        }
         const dataArr = data.map(function (v) {
             return v['tags'];
         });
@@ -32,16 +35,66 @@ router.get('/tags', function (req, res, next) {
 router.get('/videos', function (req, res, next) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query.tag;
+    var queryId = url_parts.query._id;
     if(query == ''){
-        VideoInfo.find(function (err, data) {
+        VideoInfo.find({ 'status': 'A' },function (err, data) {
+            if (err) {
+                res.status(404).send(err);
+                return;
+            }
+            res.send(data);
+        });
+    }
+    else if (queryId != undefined) {
+        VideoInfo.find({ "_id": queryId, 'status': 'A' }, function (err, data) {
+            if (err) {
+                res.status(404).send(err);
+                return;
+            }
             res.send(data);
         });
     }
     else{
-        VideoInfo.find({ "tags": query }, function (err, data) {
+        VideoInfo.find({ "tags": query, 'status': 'A' }, function (err, data) {
+            if (err) {
+                res.status(404).send(err);
+                return;
+            }
             res.send(data);
         });
     }    
+});
+
+router.put('/videos', function (req, res, next) {
+    VideoInfo.update({ '_id': req.body._id }, {
+        '$set': {
+            url: req.body.url,
+            tags: req.body.tags,
+            time: req.body.time,
+        }
+    }, function (err, doc) {
+        if (err) {
+            res.status(404).send(err);
+            return;
+        }
+        res.status(200).send({ message: "updated" })
+    })
+});
+
+router.delete('/videos', function (req, res, next) {
+    var url_parts = url.parse(req.url, true);
+    var queryId = url_parts.query._id;
+    VideoInfo.findOneAndUpdate({ '_id': queryId }, {
+        '$set': {
+            status: "I",
+        }
+    }, function (err, doc) {
+        if (err) {
+            res.status(404).send(err);
+            return;
+        }
+        res.status(200).send({ message: "deleted" })
+    })
 });
 
 router.get('/test', function (req, res, next) {
