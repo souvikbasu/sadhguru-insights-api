652de8ba89dc7beba4effff93070a2801c92972e
Ankit <ankit@qexon.com>
2018-05-15 10:30:39 +0530

    chnage api to get tags only

--- controllers/videoInfoController.js
+++ controllers/videoInfoController.js
@@ -17,8 +17,15 @@ router.post('/video', function (req, res, next) {
 });
 
 router.get('/tags', function (req, res, next) {
-    VideoInfo.find(function (err, data) {
-        res.send(data);
+    VideoInfo.find({ status: "A" }, { tags: 1, status: 1 },function (err, data) {  
+        const dataArr = data.map(function (v) {
+            return v['tags'];
+        });
+        const mergedArr = [].concat.apply([], dataArr);
+        const uniqueArray = mergedArr.filter(function (item, pos) {
+            return mergedArr.indexOf(item) === pos;
+        });
+        res.send(uniqueArray);
     });
 });
 
@@ -26,6 +33,14 @@ router.get('/videos', function (req, res, next) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query.tag;
     VideoInfo.find({ "tags": query }, function (err, data) {
+        res.send(data);
+    });
+});
+
+router.get('/all-videos', function (req, res, next) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query.tag;
+    VideoInfo.find(function (err, data) {
         res.send(data);
     });
 });
